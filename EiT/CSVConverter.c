a53f5482dd6716a02035bd044cbc2470b54e2434
larstva <larstorev@gmail.com>
2015-03-04 15:02:44 +0100

    Added option to fetch part of CSV file (from startID to endID)

--- EiT/CSVConverter.c
+++ EiT/CSVConverter.c
@@ -6,6 +6,15 @@
 #include <dos.h>  /* for delay */
 #include <conio.h>
 
+typedef int bool;
+enum { false, true };
+
+float** csvFileValues;
+int* sampleIDs;
+int lines;
+int valuesPerLine;
+const int maxLineLength = 1024;
+
 int countLines(FILE* file) {
 	int ch=0;
 	int lines=0;
@@ -25,7 +34,22 @@ int countChars( char* s, char c ) {
 	return *s == '\0' ? 0 : countChars( s + 1, c ) + (*s == c);
 }
 
-float getNextSampleNumber(FILE* file) {
+void freeCSVFileValues() {
+	int i;
+	for (i = 0; i < lines; i++) {
+	  free(csvFileValues[i]);
+	}
+	free(csvFileValues);
+}
+
+int getFirstSampleNumber(FILE* file) {
+	char line[maxLineLength];
+	fgets(line, maxLineLength, file);
+	char* firstSampleNumber = strtok(line, ",");
+	return atoi(firstSampleNumber);
+}
+
+int getLastSampleNumber(FILE* file) {
 	static const long max_len = 50;  			// define the max length of the line to read
 	char buff[max_len + 1];             		// define the buffer and allocate the length
 
@@ -36,13 +60,12 @@ float getNextSampleNumber(FILE* file) {
 	char* last_newline = strrchr(buff, '\n'); 	// find last occurrence of newlinw
 
 	char* lastSampleNumber = strtok(last_newline, ",");
-	float nextSampleNumber = strtof(lastSampleNumber, NULL) + 1;
 
 	rewind(file); // reset file position indicator to start of the file.
-	return nextSampleNumber;
+	return atoi(lastSampleNumber);
 }
 
-void writeCSVFile(long int id, float values[], int size) {
+void writeCSVFile(int id, float values[], int size) {
 
 	int i=0;
 	printf("%ld, ", id);
@@ -52,38 +75,81 @@ void writeCSVFile(long int id, float values[], int size) {
 	printf("%.2f\n", values[size-1]);
 }
 
-float** readCSVFile(FILE* file) {
-	const int maxLineLength = 1024;
+void readCSVFile(FILE* file, int sampleIdStart, int sampleIdEnd) {
+	if (csvFileValues != NULL && lines != NULL) {
+		printf("freeing csvFileValues");
+		freeCSVFileValues(); // <---------------- causes crash when run! -----------------------
+	}
 
-	int lines = countLines(file);
+	int i;
 
-	char tmpLine[maxLineLength];
-	fgets(tmpLine, maxLineLength, file);
-	int numOfLineValues = countChars( tmpLine, ',' ) + 1;
-	float values[lines][numOfLineValues];
+	if (sampleIdStart < 0 && sampleIdEnd < 0) { // Fetch whole file.
+		lines = countLines(file);
+	} else if (sampleIdStart >= 0 && sampleIdEnd < 0) { // Fetch from sampleIdStart to end of file.
+		int lastSampleId = getLastSampleNumber(file);
+		lines = lastSampleId - sampleIdStart + 1;
+	} else if (sampleIdStart < 0 && sampleIdEnd >= 0) { // Fetch from start of file to sampleIdEnd.
+		int firstSampleId = getFirstSampleNumber(file);
+		lines = sampleIdEnd - firstSampleId + 1;
+	} else { // Fetch from sampleIdStart to sampleIdEnd.
+		lines = sampleIdEnd - sampleIdStart + 1;
+	}
+
 
 	rewind(file); // reset file position indicator to start of the file.
 
 	char line[maxLineLength];
-	int i=0;
+
+	// Counts number of values on first line
+	char tmpLine[maxLineLength];
+	fgets(tmpLine, maxLineLength, file);
+	valuesPerLine = countChars( tmpLine, ',' );
+
+	sampleIDs = malloc(lines * sizeof(int));
+	csvFileValues = malloc(lines * sizeof(float*));
+	for (i = 0; i < lines; i++) {
+		csvFileValues[i] = malloc(valuesPerLine * sizeof(float));
+	}
+
+	rewind(file); // reset file position indicator to start of the file.
+
+
+	i=0;
+	int lineIdx = -1;
 	while (fgets(line, maxLineLength, file)) {
 		const char* tok;
 		int j = 0;
-		for (tok = strtok(line, ","); tok && *tok; tok = strtok(NULL, ",\n")) {
-			float value = strtof(tok, NULL); // cast value to float
-			values[i][j] = value;
-			printf("Field %i = %.2f\n", j, value);
+		bool fetchThisLine = true;
+		for (tok = strtok(line, ","); tok && *tok && fetchThisLine; tok = strtok(NULL, ",\n")) {
+			if (j == 0) { // Value is sample ID
+				int sampleID = atoi(tok);
+				if ((sampleIdStart >= 0 && sampleIdEnd < 0 && sampleID < sampleIdStart) ||
+						(sampleIdStart < 0 && sampleIdEnd >= 0 && sampleID > sampleIdEnd) ||
+						(sampleIdStart >= 0 && sampleIdEnd >=0 && (sampleID < sampleIdStart || sampleID > sampleIdEnd))) {
+					//printf("Skipping sampleID %i. ", sampleID);
+					fetchThisLine = false;
+				} else {
+					lineIdx++;
+					sampleIDs[lineIdx] = sampleID;
+				}
+			}
+
+			if (fetchThisLine) {
+				float value = strtof(tok, NULL); // cast value to float
+				csvFileValues[lineIdx][j] = value;
+				//printf("[%i][%i] = %.2f\n", i+1, j, value);
+			}
+
 			j++;
 		}
-		printf("\n");
+		if (fetchThisLine) {
+			//printf("\n");
+		}
 		i++;
 
 		free(tok);
 	}
 	rewind(file); // reset file position indicator to start of the file.
-
-	printf("test: values[10][2] = %.2f", values[10][2]);
-	return values;
 }
 
 
@@ -93,7 +159,7 @@ int main(void) {
 	// Test code for converting float to CSV:
 	/*int size = 3;
 	float sensorValues[3] = {10.423245, 24.3242, 7.9};
-	long int id;
+	int id;
 	for (id=1; id<100; id++) {
 		sensorValues[0]++;
 		sensorValues[1]++;
@@ -102,12 +168,19 @@ int main(void) {
 	}*/
 
 	// Test code for reading file and converting CSV to float:
-	//float** valuesFromCSV = readCSVFile(file);
-
+	readCSVFile(file, -1, -1); // -1 means start/end of file
+	int i, j;
+	for (i=0; i<lines; i++) {
+		printf("Sample %i: ", sampleIDs[i]);
+		for (j=0; j<valuesPerLine; j++) {
+			printf("\n%.2f", csvFileValues[i][j]);
+		}
+		printf("\n\n");
+	}
 
 	// Test code for getting the last line in the file:
-	/*float nextSampleNumber = getNextSampleNumber(file);
-	printf("next sample number: %f", nextSampleNumber);*/
+	//int nextSampleNumber = getLastSampleNumber(file) + 1;
+	//printf("Next sample number: %i", nextSampleNumber);
 
 	fclose(file);
 	return EXIT_SUCCESS;
--- EiT/CSVConverter.h
+++ EiT/CSVConverter.h
@@ -2,10 +2,10 @@
 	Converts input data to CSV format (comma separated values)
 */
 
-char** str_split(char* a_str, const char a_delim);
 int countLines(FILE* file);
 int countChars( char* s, char c );
-void writeCSVFile(long int id, float values[], int size);
-float** readCSVFile(FILE* file);
-char* getLastLineInFile(FILE* file);
-float getNextSampleNumber(FILE* file);
+void freeCSVFileValues();
+int getFirstSampleNumber(FILE* file);
+int getLastSampleNumber(FILE* file);
+void writeCSVFile(int id, float values[], int size);
+void readCSVFile(FILE* file, int sampleIdStart, int sampleIdEnd);
